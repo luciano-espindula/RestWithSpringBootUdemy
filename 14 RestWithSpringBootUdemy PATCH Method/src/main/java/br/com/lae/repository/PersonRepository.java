0d8f65f85e25e9ea277c53fd78f2447928641f72
luciano-espindula <luc.aespindula@gmail.com>
2021-03-05 11:55:44 -0300

    PATCH Method

---
+++ 14 RestWithSpringBootUdemy PATCH Method/src/main/java/br/com/lae/repository/PersonRepository.java
@@ -0,0 +1,18 @@
+package br.com.lae.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.lae.data.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+	
+	@Modifying
+	@Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
+	void disablePersons(@Param("id") Long id);
+
+}
