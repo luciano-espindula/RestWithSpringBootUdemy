8dcad5ae3ed824a0c4bf499cd3be6bf70ae7f428
luciano-espindula <luc.aespindula@gmail.com>
2021-03-30 20:20:50 -0300

    AWS with Travis CI

---
+++ 19 RestWithSpringBootUdemy AWS com Travis CI/RestWithSpringBootUdemy/src/main/java/br/com/lae/Startup.java
@@ -0,0 +1,24 @@
+package br.com.lae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+import br.com.lae.config.FileStorageConfig;
+
+@SpringBootApplication
+@EnableConfigurationProperties({FileStorageConfig.class})
+@EnableAutoConfiguration
+@ComponentScan
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+		
+//		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//		String result = bCryptPasswordEncoder.encode("root");
+//		System.out.println("My hash " + result);
+	}
+}
